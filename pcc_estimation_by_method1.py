0f3e297cefe3f808cf4d3a309230c48827985318
JINGYIWANG <116722795+JINGYIWANG1996@users.noreply.github.com>
2025-03-07 12:03:32 +0800

    Update pcc_estimation_by_method1.py

--- pcc_estimation_by_method1.py
+++ pcc_estimation_by_method1.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  6 22:00:05 2025
-
-@author: jingxiaoyi
-"""
-
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
